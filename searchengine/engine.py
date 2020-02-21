3cd305b6939eba9abf666c9f464f03d5f8183162
Ana Perisic <anaperisictb@yahoo.com>
2020-02-21 19:39:42 +0100

    #unos_upita Implementacija unosa upita

--- searchengine/engine.py
+++ searchengine/engine.py
@@ -38,16 +38,105 @@ class SearchEngine:
         return True
 
 
+
+    def unos_upita(self):
+        print(datetime.now() - start)
+        upit = input("Unesite upit:")
+
+        reci = upit.strip().split(" ")
+        for i in range(len(reci)):
+            reci[i]=reci[i].lower()
+
+        print(reci)
+        running=True
+        while running:
+            if not upit:
+                print("\t Upit: " + upit)
+                print("Pogresno je uneti prazan string.")
+                upit,reci = ponovni_unos()
+            elif (reci.count('and') + reci.count('or') + reci.count('not')) > 1:
+                print("Imamo vise od jednog logickog operatora!")
+                upit,reci = ponovni_unos()
+            elif 'and' in reci:
+                uspesno = True
+                if len(reci)==3:
+                        indeksAnd = reci.index('and')
+                        if indeksAnd!=1:
+                            print("Logicki operator AND moze samo da bude na poziciji 1!")
+                            upit, reci = ponovni_unos()
+                            uspesno = False
+                        else:
+                            #TODO izvrsiti pretragu na osnovu AND-a
+                            pass
+
+                else:
+                    print("Logicki operator AND  moze da se pojavi samo izmedju 2 reci")
+                    upit, reci = ponovni_unos()
+                    uspesno = False
+
+                if uspesno == True:
+                    running=False
+            elif 'or' in reci:
+                uspesno = True
+                if len(reci) == 3:
+                        indeksOr = reci.index('or')
+                        if indeksOr != 1:
+                            print("Logicki operator OR moze samo da bude na poziciji 1!")
+                            upit, reci = ponovni_unos()
+                            uspesno = False
+                        else:
+                            # TODO izvrsiti pretragu na osnovu OR-a
+                            pass
+
+                else:
+                    print("Logicki operator OR  moze da se pojavi samo izmedju 2 reci")
+                    upit, reci = ponovni_unos()
+                    uspesno = False
+
+                if uspesno == True:
+                    running = False
+
+            elif 'not' in reci:
+                uspesno = True
+                if len(reci) == 3:
+                        indeksNot = reci.index('not')
+                        if indeksNot != 1:
+                            print("Logicki operator NOT moze samo da bude na poziciji 1!")
+                            upit, reci = ponovni_unos()
+                            uspesno = False
+                        else:
+                            # TODO izvrsiti pretragu za NOT
+                            pass
+
+                else:
+                    print("Logicki operator NOT  moze da se pojavi samo izmedju 2 reci")
+                    upit, reci = ponovni_unos()
+                    uspesno = False
+
+                if uspesno == True:
+                    running = False
+            else:
+                running=False
+
+
+
     def search(self, query):
         # TODO parsiraj i obradi upit
         pass
 
     # TODO dodati setere za parametre za rangiranje
 
+def ponovni_unos():
+    upit = input("Greska: Nepravilan unos, unesite ponovo ispravan upit:")
+    reci = upit.strip().split(" ")
+    for i in range(len(reci)):
+        reci[i] = reci[i].lower()
+
+    return upit,reci
 
 if __name__ == "__main__":
     # TODO pokretati odavde? napraviti odvojen fajl za pokretanje? pokretati kao modul ili ne??
     start = datetime.now()
     se = SearchEngine()
     se.loadroot("C:\\Users\\Win 10\\Downloads\\python-2.7.7-docs-html")
-    print(datetime.now() - start)
+    se.unos_upita()
